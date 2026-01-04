a1d25c8399328cde5df23ca73f0de52ec2e5a4f7
Noah <noahwalshsr@gmail.com>
2026-01-04 14:39:50 -0800

    started code

---
+++ run.cpp
@@ -0,0 +1,13 @@
+//define counts for movements 1:50 gear ratio
+const int lowerc=306;
+const int upperc=612;
+const int turn=251;
+//zero encoders
+void zeroencoders(){
+    leftn=0;
+    ricghtn=0;}
+//count all encoder movements
+void encodercounts(int leftg, int rightg, int velo){
+    zeroencoders();
+    
+}
